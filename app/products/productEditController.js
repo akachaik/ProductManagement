0ab339de4d713a91f62fa4ceb0ad3f2b08d25d14
kit <308tube@gmail.com>
2015-08-12 17:23:01 +0700

    Add product search tags

--- app/products/productEditController.js
+++ app/products/productEditController.js
@@ -33,6 +33,20 @@
             $state.go('productList');
         };
 
+        vm.addTags = function(tags) {
+            if (tags) {
+                var array = tags.split(',');
+                vm.product.tags = vm.product.tags ? vm.product.tags.concat(array) : array;
+                vm.newTags = "";
+            } else {
+                alert("Please enter one or more tags seperated by commas");
+            }
+        }
+
+        vm.removeTag = function(idx) {
+            vm.product.tags.splice(idx, 1);
+        }
+
 
     }
 
